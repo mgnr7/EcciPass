1489220dec2813b839865a9d3e6337f249633e12
Manuel Fernández <mgnr7@hotmail.com>
2022-06-22 15:04:05 -0600

    Correcciones en el Footer, ClassName en vez de Class

--- ecci_pass/src/Component/PrivateRoute/index.js
+++ ecci_pass/src/Component/PrivateRoute/index.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRoute({ children, redirectPath = "/login" }) {
   const userState = useSelector((state) => state.user);
-  console.log(userState.isLoggedIn);
+  console.log("User LoggedIn: ", userState.isLoggedIn);
   if (!userState.isLoggedIn) {
     return <Navigate to={redirectPath} replace />;
   }
